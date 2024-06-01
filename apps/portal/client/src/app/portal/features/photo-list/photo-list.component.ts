95c51aee886995a98055ae1d29d47f7f5fe1aaf7
volong.workspace <volong.workspace@gmail.com>
2024-06-01 21:42:37 +0700

    create portal-client project

---
+++ apps/portal/client/src/app/portal/features/photo-list/photo-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './photo-list.component.html',
+  styleUrl: './photo-list.component.css'
+})
+export class PhotoListComponent {
+
+}
